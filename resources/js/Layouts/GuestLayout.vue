0f5e225439f615dcd399e25e2405c8f63ba2c15b
Mohammed H. Al-Swesy <melswesy6@gmail.com>
2026-01-15 07:25:46 +0200

    Initial commit - WorkZone

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -1,5 +1,4 @@
 <script setup>
-import ApplicationLogo from '@/Components/ApplicationLogo.vue';
 import { Link } from '@inertiajs/vue3';
 </script>
 
@@ -7,14 +6,14 @@ import { Link } from '@inertiajs/vue3';
     <div
         class="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0"
     >
-        <div>
-            <Link href="/">
-                <ApplicationLogo class="h-20 w-20 fill-current text-gray-500" />
-            </Link>
+        <!-- تم حذف الشعار -->
+        <!-- تقدر تضيف اسم WorkZone بدل الصورة -->
+        <div class="mb-4 text-2xl font-bold text-indigo-600">
+            WorkZone
         </div>
 
         <div
-            class="mt-6 w-full overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg"
+            class="mt-2 w-full overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg"
         >
             <slot />
         </div>
